a3e32cc7497b3ba704ed2b207ef202bf8c052b92
matiasgarcia91 <matiasigarcia91@gmail.com>
2022-10-18 03:46:15 +0800

    Set up Express, TS and graphql

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import dotenv from "dotenv";
+import cors from "cors";
+import http from "http";
+import { typeDefs } from "./graphql/schema";
+import { resolvers } from "./graphql/resolvers";
+dotenv.config();
+async function startServer() {
+    const app = express();
+    const PORT = process.env.PORT;
+    const httpServer = http.createServer(app);
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    await server.start();
+    app.use("/graphql", cors(), express.json(), expressMiddleware(server, {
+        context: async ({ req }) => ({ token: "hola" }),
+    }));
+    app.get("/hello", (req, res) => {
+        res.send("Hello");
+    });
+    await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+    console.log(`🚀 Server ready at http://localhost:${PORT}/`);
+}
+startServer();
